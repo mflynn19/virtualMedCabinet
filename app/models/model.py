1cbc3f28c91e6bc98555c2cc51578492e0aad68e
EC2 Default User <ec2-user@ip-172-31-25-170.us-east-2.compute.internal>
2019-08-01 15:42:30 +0000

    more stuff

---
+++ app/models/model.py
@@ -0,0 +1,11 @@
+from app import app
+from flask_pymongo import PyMongo
+from pymongo import MongoClient
+
+app.config['MONGO_DBNAME'] = 'virtualMedCabinet' 
+app.config['MONGO_URI'] = 'mongodb+srv://admin:Hjrf6Twqr304AJHq@cluster0-3hcwl.mongodb.net/virtualMedCabinet?retryWrites=true&w=majority'
+mongo = PyMongo(app) 
+
+client = MongoClient("mongodb+srv://admin:Hjrf6Twqr304AJHq@cluster0-3hcwl.mongodb.net/virtualMedCabinet?retryWrites=true&w=majority")
+db = client['virtualMedCabinet']
+collection = mongo.db.medications
