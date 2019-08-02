ca4c1af3387854ebb464f94705e733dcdf0277a4
EC2 Default User <ec2-user@ip-172-31-25-170.us-east-2.compute.internal>
2019-08-02 02:49:54 +0000

    style and cleanup

--- app/routes.py
+++ app/routes.py
@@ -19,12 +19,26 @@ def index():
 def addNew():
     return render_template("addNew.html")
     
+@app.route('/delMed', methods = ['GET', 'POST'])
+def delMed():
+    if request.method == 'POST':
+        formData = dict(request.form)
+        collection = mongo.db.medications
+        collection.delete_one({"name":formData["delName"]})
+        meds = list(collection.find({}))
+        return render_template("delMed.html", meds = meds)
+    else:
+        collection = mongo.db.medications
+        meds = list(collection.find({}))
+        return render_template("delMed.html", meds = meds)
+    
 @app.route('/yourMeds', methods = ['GET', 'POST'])
 def yourMeds():
     if request.method == 'POST':
         formData = dict(request.form)
         collection = mongo.db.medications
-        collection.insert({"name": formData["medName"], "purpose":formData["medPurpose"], "doc":formData["doctor"], "amount":formData["medCount"], "type":formData["medType"],"expiration":formData["medExp"] })
+        #makes keyError on medName for medDel??????????
+        collection.insert({"name": formData["medName"], "purpose":formData["medPurpose"], "patient":formData["medPatient"], "doc":formData["doctor"], "amount":formData["medCount"], "type":formData["medType"],"expiration":formData["medExp"] })
         meds = list(collection.find({}))
         return render_template("yourMeds.html", meds = meds)
     else:
@@ -37,7 +51,7 @@ def adjustMed():
     if request.method == 'POST':
         formData = dict(request.form)
         collection = mongo.db.medications
-        collection.replace_one({"name": formData["drugName"]}, {"name": formData["drugName"], "purpose":formData["drugPurpose"], "doc":formData["drugDoctor"], "amount":formData["drugAmount"], "expiration":formData["drugExp"]})
+        collection.replace_one({"name": formData["drugName"]}, {"name": formData["drugName"], "purpose":formData["drugPurpose"],"patient":formData["drugPatient"], "doc":formData["drugDoctor"], "amount":formData["drugAmount"], "expiration":formData["drugExp"]})
         meds = list(collection.find({}))
         return render_template("adjustMed.html", meds = meds)
     else:
@@ -57,5 +71,3 @@ def pharmacyLocator():
         collection = mongo.db.medications
         zippy = list(collection.find({}))
         return render_template("pharmacyLocator.html", zippy = zippy)
-        
-#get object id to query DB, jinja2{{}} teplate to set values based on key ({{data.doctor}}), post request to update entry for everything; update({{what to find in db}}, {{field to update use from form}}) collection.find_one_and_update gets the first instance and updates find_one({'_id': ObjectId(string version of ID)})
